0700851978a99f7025f144891a579bd6ab52c61b
ArturoDuCas <a00833516@tec.mx>
2024-01-17 14:44:47 -0600

    feat: create responsive navbar

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,38 +1,58 @@
-import React from 'react';
+"use client";
+import React, {useState} from 'react';
 
 import styles from "./links.module.css";
 import NavLink from "@/components/navbar/links/navLink/navLink";
 
-const Links = () => {
+const links = [
+  { href: '/', label: 'Homepage' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' },
+  { href: '/blog', label: 'Blog' },
+];
 
-  const links = [
-    { href: '/', label: 'Homepage' },
-    { href: '/about', label: 'About' },
-    { href: '/contact', label: 'Contact' },
-    { href: '/blog', label: 'Blog' },
-  ];
+const Links = () => {
+  const [open, setOpen] = useState(false);
 
   // TODO: Replace these temporary variables with real ones
   const session = true;
   const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link, index) => {
-        return (
-          <NavLink key={index} item={link} />
-        );
-      })}
-      {session ?
-        ( // If session is active
-          <>
-            {isAdmin && <NavLink item={{ href: '/admin', label: 'Admin' }} />}
-            <button className={styles.logout}>Logout</button>
-          </>
-        ) :
-        (
-          <NavLink item={{ href: '/login', label: 'Login' }} />
-        )
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link, index) => {
+          return (
+            <NavLink key={index} item={link} />
+          );
+        })}
+        {session ?
+          ( // If session is active
+            <>
+              {isAdmin && <NavLink item={{ href: '/admin', label: 'Admin' }} />}
+              <button className={styles.logout}>Logout</button>
+            </>
+          ) :
+          (
+            <NavLink item={{ href: '/login', label: 'Login' }} />
+          )
+        }
+      </div>
+
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen(prev => !prev)}
+      >
+        Menu
+      </button>
+      { open &&
+        <div className={styles.mobileLinks}>
+          {links.map((link, index) => {
+             return (
+               <NavLink key={index} item={link} />
+             );
+          })}
+        </div>
       }
     </div>
   );
