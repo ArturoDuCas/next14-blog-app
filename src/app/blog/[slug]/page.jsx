8df549801279751f58f50b9bed981a184ef19d99
ArturoDuCas <a00833516@tec.mx>
2024-01-19 20:50:53 -0600

    feat: create single post page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import styles from './postPage.module.css';
+import Image from "next/image";
 
 const PostPage = () => {
   return (
-    <div>
-      PostPage
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image className={styles.img} src='https://images.pexels.com/photos/13798633/pexels-photo-13798633.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt="post image" fill/>
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.details}>
+          <Image className={styles.avatar} src='/noavatar.png' alt="author image" height={30} width={30}/>
+          <div className={styles.detailsText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>John Doe</span>
+          </div>
+          <div className={styles.detailsText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores commodi cumque dolorem ea nihil placeat praesentium recusandae soluta voluptas! Amet commodi explicabo fuga ipsa iusto modi necessitatibus tempore voluptas?
+        </div>
+
+
+      </div>
     </div>
   );
 };
